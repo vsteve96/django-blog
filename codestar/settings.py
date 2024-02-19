146d2446dd7e3909b8168062a70bdfd9976da870
Istvan Vizhanyo <vizhanyo.istvan0@gmail.com>
2024-02-19 01:02:28 +0000

    Prepared for deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&w*+b03=h1dlc7r*xcnamt(1it1wqns(ak)j-wirm&4i*9q4bn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
